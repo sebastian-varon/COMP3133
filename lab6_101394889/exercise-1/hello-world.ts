734ba805359361b069e01c38fcc9262b6199ac80
sebastian-varon <sebasvaronmoreno@hotmail.com>
2025-03-06 15:21:06 -0500

    Lab 6 - Completed

---
+++ lab6_101394889/exercise-1/hello-world.ts
@@ -0,0 +1,5 @@
+let greeter = (firstName: string, lastName: string) => {
+    console.log(`Hello ${firstName} ${lastName}`);
+  };
+  
+  greeter("Seb", "Varon");  
---
+++ lab6_101394889/exercise-4/main.ts
@@ -0,0 +1,5 @@
+import { Customer } from './customer';
+
+let customer = new Customer("Seb", "Varon", 21);
+customer.greeter();
+customer.GetAge();
